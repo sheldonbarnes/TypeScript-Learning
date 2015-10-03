b62160db79fb8f4928ed1e4bdcb6ec942d315067
Sheldon Barnes <sheldonbarnes@icloud.com>
2015-10-03 15:00:44 -0400

    Initial commit

---
+++ Learn.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sheldonbarnes on 10/3/15.
+ */
+var Product = (function () {
+    function Product(productId) {
+        this.productId = productId;
+    }
+    return Product;
+})();
+var poop = new Product(12);
+poop.productId = 12;
+//# sourceMappingURL=Learn.js.map
---
+++ Learn.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by sheldonbarnes on 10/3/15.
+ */
+
+
+class Product {
+
+    productId: number;
+
+    constructor (productId: number) {
+        this.productId = productId;
+    }
+}
+
+
+var poop = new Product(12);
+
+poop.productId = 12;
+
+
+
+
+
